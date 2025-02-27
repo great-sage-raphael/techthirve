e7d43706ad7e6c55fd0d1a659c940cfea7f24b75
vinayak prakash <vinayakprakash2004@gmail.com>
2025-02-27 20:53:10 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import { TypewriterEffect } from "./components/ui/typewriter-effect";
+import { TimelineCarbon } from "./components/catlog";
+
+import KeralaForestVisualization from "./components/keralavisualisation";
+import Treeloss from "./components/Treeloss";
+import { BackgroundBoxes } from "./components/Backgroundbox";
+
+
+export default function Home() {
+  const words=[{
+    text:'Carbon',
+  },{
+    text:" Emissions ",
+  },{
+    text:" and ",
+  },{
+    text:" Tree ",
+  },{
+    text:" cover ",
+  },{
+    text:" loss ",
+  }]
+  const content= ['emission','treeloss']
+  return (
+    <>
+      <main className="flex justify-center items-center ">
+      <div className="flex flex-col items-center justify-center h-[40rem] ">
+     <BackgroundBoxes/>
+     
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
+      
+      </div>
+      
+    </div>
+      </main>
+      <div>
+      <TimelineCarbon/>
+      </div>
+     
+     
+    </>
+  );
+}
